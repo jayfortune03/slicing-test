df63058224c85af6634eb59d3ef461f98467e7ad
Nicholas Fortune <nfortune03@gmail.com>
2021-10-01 22:07:29 +0700

    login page styling almost done, ongoing layouting

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -10,19 +10,37 @@ export default function LoginPage() {
           <div className="flex flex-wrap w-full mb-4 relative h-15 bg-white items-center rounded pr-10">
             <div className="flex -mr-px justify-center w-15 p-4">
               <span className="flex items-center leading-normal bg-white px-3 border-0 rounded rounded-r-none text-2xl text-gray-600">
-                <i className="fas fa-user-circle"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-envelope text-gray-400"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z" />
+                </svg>
               </span>
             </div>
             <input
-              type="text"
+              type="email"
               className="flex-shrink flex-grow flex-auto leading-normal w-px border-0 h-10 border-grey-light rounded rounded-l-none px-3 self-center relative  font-roboto text-xl outline-none"
-              placeholder="Username"
+              placeholder="Email"
             />
           </div>
           <div className="flex flex-wrap w-full relative h-15 bg-white items-center rounded mb-4">
             <div className="flex -mr-px justify-center w-15 p-4">
               <span className="flex items-center leading-normal bg-white rounded rounded-r-none text-xl px-3 whitespace-no-wrap text-gray-600">
-                <i className="fas fa-lock"></i>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  className="bi bi-lock text-gray-400"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2zM5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1z" />
+                </svg>
               </span>
             </div>
             <input
@@ -30,15 +48,10 @@ export default function LoginPage() {
               className="flex-shrink flex-grow flex-auto leading-normal w-px border-0 h-10 px-3 relative self-center font-roboto text-xl outline-none"
               placeholder="Password"
             />
-            <div className="flex -mr-px">
-              <span className="flex items-center leading-normal bg-white rounded rounded-l-none border-0 px-3 whitespace-no-wrap text-gray-600">
-                <i className="fas fa-eye-slash"></i>
-              </span>
-            </div>
           </div>
           <a
             href="#"
-            className="text-base font-bold mr-48 pt-6 text-white text-right leading-normal hover:underline mb-6"
+            className="text-base font-bold mr-48 text-white text-right leading-normal hover:underline mb-2 mt-2"
           >
             Forgot Password
           </a>
@@ -48,16 +61,16 @@ export default function LoginPage() {
           >
             Sign In
           </a>
-          <h1 className="font-black text-white">
-            Didn't have any account ?{" "}
-            <a
-              href="#"
-              className="text-base font-black mr-48 pt-6 text-green-400 text-right leading-normal underline mb-6"
-            >
-              Sign up here
-            </a>
-          </h1>
         </form>
+        <h1 className="font-black ml-2 shadow-lg text-white">
+          Didn't have any account ?{" "}
+          <a
+            href="#"
+            className="text-base font-black mr-48 pt-6 text-green-400 text-right leading-normal underline mb-6"
+          >
+            Sign up here
+          </a>
+        </h1>
       </div>
     </div>
   );
