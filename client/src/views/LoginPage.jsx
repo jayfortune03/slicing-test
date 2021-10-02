6c6c21e492329f9bed98762815a1b651c542530a
Nicholas Fortune <nfortune03@gmail.com>
2021-10-02 16:06:23 +0700

    login page done

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -3,14 +3,17 @@ import mail from "../assets/mail.png";
 
 export default function LoginPage() {
   return (
-    <div className="flex bg-black">
+    <div className="flex flex-col">
+      <h1 className="text-6xl flex text-black font-bold mx-auto my-6">
+        Login Page
+      </h1>
       <div className="artboard phone-4 mx-auto my-auto login artboard-demo">
         <h1 className="font-bold text-5xl mb-4 mt text-white">Audio</h1>
         <h1 className="font-regular text-md mb-48 flex text-white">
           It's modular and designed to last
         </h1>
         <form action="" className="flex flex-col">
-          <div className="flex flex-wrap w-full mb-4 relative h-15 bg-white items-center rounded-xl pr-10">
+          <div className="flex flex-wrap w-full mb-6 relative h-15 bg-white items-center rounded-xl pr-10">
             <div className="flex -mr-px p-4 justify-center">
               <span className="flex items-center leading-normal bg-white px-3 border-0 rounded rounded-r-none text-2xl text-gray-600">
                 <img src={mail} width="24" height="24" />
@@ -18,11 +21,11 @@ export default function LoginPage() {
             </div>
             <input
               type="email"
-              className="flex-shrink flex-grow flex-auto leading-normal w-56 border-0 h-10 border-grey-light rounded rounded-l-none px-3 self-center relative  font-roboto text-md outline-none"
+              className="flex-shrink flex-grow flex-auto leading-normal w-56 border-0 h-10 border-grey-light rounded rounded-l-none px-3 self-center relative text-md outline-none"
               placeholder="Email"
             />
           </div>
-          <div className="flex flex-wrap w-full relative h-15 bg-white items-center rounded-xl mb-4">
+          <div className="flex flex-wrap w-full relative h-15 bg-white items-center rounded-xl mb-6">
             <div className="flex -mr-px p-4 justify-center">
               <span className="flex items-center leading-normal bg-white rounded rounded-r-none text-xl px-3 whitespace-no-wrap text-gray-600">
                 <img src={lock} width="24" height="24" />
@@ -30,7 +33,7 @@ export default function LoginPage() {
             </div>
             <input
               type="password"
-              className="flex-shrink flex-grow flex-auto leading-normal w-56 border-0 h-10 px-3 relative self-center font-roboto text-md outline-none"
+              className="flex-shrink flex-grow flex-auto leading-normal w-56 border-0 h-10 px-3 relative self-center text-md outline-none"
               placeholder="Password"
             />
           </div>
@@ -42,7 +45,7 @@ export default function LoginPage() {
           </a>
           <a
             href="/"
-            className="signin-btn font-bold py-4 text-center px-4 text-white rounded-xl leading-tight mt-4 mb-4"
+            className="signin-btn font-bold py-4 text-center px-4 text-white rounded-xl leading-tight mt-4 mb-6"
           >
             Sign In
           </a>
